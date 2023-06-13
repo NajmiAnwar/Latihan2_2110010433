2540ead9e8cc090f1ef5fa02fd13943960bd6226
NajmiAnwar <njillenn@gmail.com>
2023-06-13 09:52:52 +0800

    Menjalankan Program

--- src/universitas/Mahasiswa.java
+++ src/universitas/Mahasiswa.java
@@ -1,14 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package universitas;
 
-/**
- *
- * @author Najmi
- */
 public class Mahasiswa {
+    private int nim;
+    private String nama, prodi;
+
+    public int getNim() {
+        return nim;
+    }
+
+    public void setNim(int nim) {
+        this.nim = nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getProdi() {
+        return prodi;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+
+    
+    
+    public void viewdata(){
+        System.out.println("Nama  : \t"+ nama);
+        System.out.println("NIM   : \t"+ nim);
+        System.out.println("Prodi : \t"+ prodi);
+    }
+
     
 }
--- src/universitas/MahasiswaBeraksi.java
+++ src/universitas/MahasiswaBeraksi.java
@@ -1,14 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package universitas;
+import universitas.Mahasiswa;
 
-/**
- *
- * @author Najmi
- */
 public class MahasiswaBeraksi {
+    public static void main(String[] args) {
+        Mahasiswa data = new Mahasiswa();
+        data.setNama("MUHAMMAD NAJMI ANWAR");
+        data.setNim(2110010433);
+        data.setProdi("TI");
+        data.viewdata();
+        
+    }
     
 }
